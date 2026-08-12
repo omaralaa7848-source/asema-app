39eb83ac64ce7bb2ecabfda5b81a10308bb68502
Omar Alaa <omaralaa7848@users.noreply.github.com>
2026-08-12 21:24:22 +0300

    @ نسخة 1.2.0 (build 18) — الرصيد والاستبدال والشاشات الجديدة

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3456830151" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1638327404" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
