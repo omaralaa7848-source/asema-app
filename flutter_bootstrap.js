4f4c48aa5203d225dc86b6f769942c2667936dc9
Omar Alaa <omaralaa7848@users.noreply.github.com>
2026-08-13 10:44:39 +0300

    @ نسخة 1.2.3 (build 21) — تأكيد المندوب إجباري + بحث المناديب

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4004468282" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4260090976" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
