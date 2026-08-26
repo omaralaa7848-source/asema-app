1eabcb79e7fedf451398f3b55b4a48a1f66b22ae
omaralaa7848-source <omaralaa7848@gmail.com>
2026-08-26 17:37:39 +0300

    2.4.3 build 51 — إصلاح اختفاء المرتجعات المستنية استلام + زرار سلّمت

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2842853203" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "943481516" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
